5a454c0bc49aa920f88730e1a7ae2b4dcc21ea7e
Szymon Brycki <szymon.brycki@gmail.com>
2023-03-25 18:17:33 +0100

    Add files via upload

---
+++ SysData.py
@@ -0,0 +1,34 @@
+import platform
+
+class SysData():
+    '''
+    Class that prints computer system data.
+    '''
+
+    def printData(self):
+        '''
+        Method that prints computer system data.
+        
+        ARGUMENTS:
+
+        Nothing
+
+        RETURNS:
+
+        Nothing (BUT prints the data)
+        '''
+
+        print("System information:")
+
+        SysInfo = platform.uname()
+
+        print("OS: {}".format(SysInfo[0]))
+        print("OS release: {}".format(SysInfo[2]))
+        print("OS version: {}".format(SysInfo[3]))
+        print("Machine type: {}".format(SysInfo[4]))
+        print("Machine name: {}".format(SysInfo[1]))
+
+if __name__ == "__main__":
+    oSysData = SysData()
+    oSysData.printData()
+
